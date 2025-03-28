7d40cb50163d5f8e9fb603bf8dbcf7d3f2cce48f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-28 20:29:36 -0300

    Aula de js 2

--- Script.js
+++ Script.js
@@ -23,7 +23,7 @@ console.timeEnd('Timer');
 */
 
 //variaveis let
-
+/*
 let texto = "Olá"
 
 let numero = 42
@@ -39,3 +39,57 @@ let uniqueId = Symbol ("id")
 let bigNumero = 99999999999999n;
 
 console.log(texto, typeof texto); 
+*/
+/*
+let task = [
+    { id: 1, descrição: "aula de js" },
+    { id: 2, descrição: "aula de html"},
+    { id: 3, descrição: "aula de python"},
+]
+*/
+//fazer array aq
+
+/*
+let task = { id: 1, descrição: "aula de JS"
+, data: new data
+}*/
+
+//
+
+/*let a = 10
+let b = 3
+
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+*/
+
+//js 2
+
+//let tarefaConcluida = true
+//if ( tarefaConcluida) {
+  //  console.log("A tarefa esta concluida")}
+
+  /*switch (key) {
+    case value:
+        
+        break;
+  
+    default:
+        break;
+  }*/
+
+  /*for (let i = 0; i >= 10; i++) {
+    console.log("Contagem", i)
+  }*/
+
+ /*let contador = 0
+
+  while (contador <5) {
+    console.log("contagem", contador)
+    contador++;
+  }*/
+
+  
