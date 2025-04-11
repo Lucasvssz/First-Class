d7fb4f15d409b3d328759ff45ca57cad6f1d6004
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-11 20:36:00 -0300

    Aula 3

--- Script.js
+++ Script.js
@@ -70,27 +70,27 @@ console.log()
 
 //let tarefaConcluida = true
 //if ( tarefaConcluida) {
-  //  console.log("A tarefa esta concluida")}
+//  console.log("A tarefa esta concluida")}
 
-  /*switch (key) {
-    case value:
-        
-        break;
-  
-    default:
-        break;
-  }*/
+/*switch (key) {
+  case value:
+      
+      break;
+ 
+  default:
+      break;
+}*/
 
-  /*for (let i = 0; i >= 10; i++) {
-    console.log("Contagem", i)
-  }*/
+/*for (let i = 0; i >= 10; i++) {
+  console.log("Contagem", i)
+}*/
 
- /*let contador = 0
+/*let contador = 0
 
-  while (contador <5) {
-    console.log("contagem", contador)
-    contador++;
-  }*/
+ while (contador <5) {
+   console.log("contagem", contador)
+   contador++;
+ }*/
 
 
 /*let pattern = /^\+55\(\d{2}\)\d{4,5}\-\d{4}$/;
@@ -100,7 +100,7 @@ console.log()
     console.log(pattern.test("+55(11)5514-2689"))*/
 
 
-    //Aula 3
+//Aula 3
 
 /*let trabalhoFeito = true; 
 
@@ -149,3 +149,121 @@ while (contador < 5) {
 }*/
 
 
+/*let titulo = "aprender java"
+let descrição = "Estudar manipulação"
+
+console.log(titulo, typeof (titulo))
+console.log(descrição, typeof (descrição))
+
+console.log("Tamanho", titulo.length)
+console.log("Tamanho da descrição", descrição.length)
+
+const palavra = "palavra"
+console.log("caracter selctor", palavra.charAt (3))*/
+
+
+
+/*let categoria = "estudo"
+
+let infoCompleta = "Categoria" = categoria + categoria + " - " + titulo
+console.log(categoria)*/
+
+
+
+/*let texto1  = "Posição do javascript "
+
+console.log(texto1.indexOf("javacript"))
+
+console.log(texto1.startsWith(Posição))//endswith mesma coisa*/
+
+
+/*let texto = "javascript,programação,front,web "
+
+console.log("texto original", texto)
+
+console.log("Texto slice", texto.slice(0, 10))
+console.log("texto sub", texto.substring(0, 10))
+
+let arrayTags = texto.split(",")
+
+console.table(arrayTags)
+
+console.log (arrayTags.join(","))*/
+
+/*const destacarTermo = (texto, termo) => {
+  if (!termo) return termo
+  const regex = new RegExp(termo, "gi")
+
+  return texto.replace(regex, `**$&**`)
+
+  let busca = destacarTermo("Javascript é uma linguagem incrível, amo java script ")
+  cosnole.log ("Resultado", busca)
+
+ }
+
+
+ console.log("math.pi", Math.pi)
+ console.log("math.E", Math.E)
+
+ const raio = 5 
+
+ const areaCirculo = Math.pi + Math.pow(raio, 2)*/
+
+
+/* const numero = 9.7
+
+ console.log(Math.round(numero))
+ console.log(Math.floor(numero))
+ console.log(Math.ceil(numero))
+ console.log(Math.trunc(numero))*/
+
+
+/*const numeroAleatorioentre = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1 ) + min) 
+}
+
+consle.log(numeroAleatorioentre(1, 100))*/
+
+
+
+/*const hoje =  new Date()
+
+console.log(new Date("2025-04-12"))
+console.log(new Date("04-12-2025T12:00"))*/
+
+/*const hoje = new Date()
+
+const formatarData = (data) => {
+  const dia = data.getDate().tostring().padStart(2, "0");
+  const mes = (data.getMonth() +1 ).todtring()
+  const ano = data.getFullYear()
+
+  return`$(dia);$(mes);$(ano)`
+}
+
+console.log(hoje.getDate())
+console.log(hoje.getMonth())
+console.log(hoje.getFullYear())
+
+console.log(hoje )
+
+console.log(formatarData(hoje))
+
+const adicionarDias = (data, dia) => {
+    const novaData = new Date(data)
+
+    novaData.setDate
+}*/
+
+
+const hoje = new Date ();
+
+const dataFinal = new Date("05-11-2025")
+
+const diferencaMs = dataFinal - hoje
+
+console.log(diferencaMs)
+
+const diferencaDias = Math.ceil(diferencaMs / (1000 * 60 * 60 * 24))
+
+console.log("Diferença em dias:", diferencaMs)
